719cbd2eb48c3b78884c6bc6dce95770e289fdac
BahaBakan <bahabakan7@gmail.com>
2025-10-07 21:56:34 +0300

    Google maps kullanılarak kullanıcının haritalarda konum seçip kaydedebileceği,silebileceği,listeleyebileceği mobil uygulama projesi

---
+++ app/src/main/java/com/bahabakan/javamaps/adapter/PlaceAdapter.java
@@ -0,0 +1,62 @@
+package com.bahabakan.javamaps.adapter;
+
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bahabakan.javamaps.databinding.RecyclerRowBinding;
+import com.bahabakan.javamaps.model.Place;
+import com.bahabakan.javamaps.view.MapsActivity;
+
+import java.util.List;
+
+public class PlaceAdapter extends RecyclerView.Adapter<PlaceAdapter.PlaceHolder> {
+
+    List<Place> placeList;
+
+    public PlaceAdapter(List<Place> placeList) {
+        this.placeList = placeList;
+    }
+
+    @NonNull
+    @Override
+    public PlaceHolder  onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        RecyclerRowBinding recyclerRowBinding = RecyclerRowBinding.inflate(LayoutInflater.from(parent.getContext()),parent,false);
+        return new PlaceHolder(recyclerRowBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PlaceHolder  holder, int position) {
+        holder.recyclerRowBinding.recyclerViewTextView.setText(placeList.get(position).name);
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(holder.itemView.getContext(), MapsActivity.class);
+                intent.putExtra("info","old"); // eski bir veri gönderileceğini haber veriyorum.
+                intent.putExtra("place",placeList.get(position));
+                holder.itemView.getContext().startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() { // kaç tane mekan varsa o kadarlık rcycler row oluşturur.
+        return placeList.size();
+    }
+
+    public class PlaceHolder extends RecyclerView.ViewHolder {
+
+    RecyclerRowBinding recyclerRowBinding;
+
+    public PlaceHolder(RecyclerRowBinding recyclerRowBinding) {
+        super(recyclerRowBinding.getRoot());
+        this.recyclerRowBinding = recyclerRowBinding;
+    }
+}
+
+}
